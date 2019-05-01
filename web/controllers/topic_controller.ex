551c9ea4d101e311918b4420764d0921fdd026b0
Anthony Rudny <anthonyrudy@Anthonys-MacBook-Pro.local>
2019-05-01 07:11:40 -0500

    models connected to postgres

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,20 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(_conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
+  end
+end
