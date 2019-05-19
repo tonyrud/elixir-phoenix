e4f2403f8d2f30385bc3e002987130d687f68c82
Anthony Rudny <anthonyrudny@Anthony-Rudny.local>
2019-05-19 17:01:25 -0400

    editing topics, deleting topics setup

--- web/router.ex
+++ web/router.ex
@@ -2,23 +2,28 @@ defmodule Discuss.Router do
   use Discuss.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", Discuss do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    get("/", TopicController, :index)
+    get("/topics/new", TopicController, :new)
+    get("/topics/:id/edit", TopicController, :edit)
+    post("/topics", TopicController, :create)
+    put("/topics/:id", TopicController, :update)
+    delete("/topics/:id", TopicController, :delete)
+    # resources("/", TopicController)
   end
 
   # Other scopes may use custom stacks.
