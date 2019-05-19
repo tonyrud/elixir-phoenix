35e4668c5a2f36eec802452ccb11f7b2682ef92e
Anthony Rudny <anthonyrudny@Anthony-Rudny.local>
2019-05-19 16:29:30 -0400

    new topic, all topics setup

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,20 +1,32 @@
 defmodule Discuss.TopicController do
+  # gives tons of web helpers and ecto connections ie.. Repo
   use Discuss.Web, :controller
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
+
+  # conn is our req/res information
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(_conn, %{"topic" => topic}) do
+  def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
-      {:error, changeset} -> IO.inspect(changeset)
+      {:ok, _post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
     end
   end
 end
