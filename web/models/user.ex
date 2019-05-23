ba2b21133344db5d3fd8ad732828426189c3c81c
tonyrud <tonrudny@gmail.com>
2019-05-22 22:42:44 -0400

    changes to auth setup

--- web/models/user.ex
+++ web/models/user.ex
@@ -8,7 +8,7 @@ defmodule Discuss.User do
     field :provider, :string
     field :token, :string
     has_many :topics, Discuss.Topic
-    has_many :comments, Discuss.Comment
+    # has_many :comments, Discuss.Comment
 
     timestamps()
   end
