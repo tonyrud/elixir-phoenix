a43737f35d5f75eb871d8903b350bdace220572e
tonyrud <tonrudny@gmail.com>
2019-05-27 11:25:51 -0400

    feat: plugs created

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -1,13 +1,14 @@
 defmodule Discuss.Topic do
-    use Discuss.Web, :model
+  use Discuss.Web, :model
 
-    schema "topics" do
-        field :title, :string
-    end
+  schema "topics" do
+    field(:title, :string)
+    belongs_to(:user, Discuss.User)
+  end
 
-    def changeset(struct, params \\ %{}) do
-        struct
-            |> cast(params, [:title])
-            |> validate_required([:title])
-    end
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
 end
